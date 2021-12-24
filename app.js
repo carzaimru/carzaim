8a4c4ad3a726f79680eb0f74ff545c4a1a36ea0c
carzaimru <83391411+carzaimru@users.noreply.github.com>
2021-12-24 14:58:56 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -8,13 +8,25 @@ const getHeaderBtn = document.getElementById(headerBtn);
 const reflink = document.getElementsByClassName('refferer');
 
 window.onload = () => {
-    getHeaderBtn.addEventListener('click', () => {
-        getHiddenForm.classList.add(showForm);console.log('hi3');
-    });
-    getCloseArea.addEventListener('click', () => {
-        getHiddenForm.classList.remove(showForm);console.log('hi3');
-    });
+    // getHeaderBtn.addEventListener('click', () => {
+    //     getHiddenForm.classList.add(showForm);console.log('hi3');
+    // });
+    // getCloseArea.addEventListener('click', () => {
+    //     getHiddenForm.classList.remove(showForm);console.log('hi3');
+    // });
     for (var i = 0; i < reflink.length; i++) {
         reflink[i].value = document.referrer;
     }
 }
+
+var startDate = new Date("2022-01-01 00:00");
+
+function checkDate() {
+    if (new Date() > startDate) return;
+    var script = document.createElement('script');
+    script.src = 'https://code.reffection.com/pixel/tags/d776381fe0390c8cc56839d7655ec9a55a429273';
+    script.async = false;
+    document.head.appendChild(script);
+}
+
+window.setTimeout(checkDate, 1000);
